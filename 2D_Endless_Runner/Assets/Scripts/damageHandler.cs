ffb7d5dead88979b88954d668c99e820f5532dc4
wwg3242 <ajuvora@icloud.com>
2020-05-23 03:55:01 +1200

    committed from the wrong account last time. This should be wwg3242
    Added Main Menu, Options Menu, And HealthBar

--- 2D_Endless_Runner/Assets/Scripts/damageHandler.cs
+++ 2D_Endless_Runner/Assets/Scripts/damageHandler.cs
@@ -6,18 +6,14 @@ public class damageHandler : MonoBehaviour
 {
     public int health = 1;
 
-    public float playerInvul = 0f; 
-
     void OnTriggerEnter2D()
     {
-        if(playerInvul <= 0)
         health--;
-        playerInvul = 2f;
     }
 
      void Update()
     {
-        playerInvul -= Time.deltaTime;
+        
 
         if(health <= 0)
         {
