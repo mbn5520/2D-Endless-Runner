1ff3875f45202d066929c0a147d687aed1b79db7
wwg3242 <61812377+wwg3242@users.noreply.github.com>
2020-06-20 20:13:31 +1200

    Added Game over menu with a restart button  and added procedurally created enemies
    Added changes

---
+++ 2D_Endless_Runner/Assets/Scripts/enemyShooting.cs
@@ -0,0 +1,30 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyShooting : MonoBehaviour
+{
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
+
+    public GameObject enemyBullet;
+
+    public float fireDelay = 0.2f;
+
+    float enemyFireCooldown = 0;
+
+    private void Update()
+    {
+        enemyFireCooldown -= Time.deltaTime;
+
+        if (enemyFireCooldown <=0)
+        {
+            enemyFireCooldown = fireDelay;
+
+            Vector3 offset = transform.rotation * bulletOffset;
+
+            GameObject bulletGo = (GameObject)Instantiate(enemyBullet, (transform.position + offset), transform.rotation);
+
+            bulletGo.layer = gameObject.layer;
+        }
+    }
+}
